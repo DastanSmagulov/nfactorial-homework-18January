1f5ec5af2bb65dea54fdc435b05a3e5efe0c1d8f
Dastan <73186335+DastanSmagulov@users.noreply.github.com>
2023-01-26 11:40:55 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let table = document.getElementById("age-table");
+
+table.getElementsByTagName("label");
+
+table.getElementsByTagName("td")[0];
+
+document.querySelector('form[name="search"]');
+
+form.querySelector("input");
+
+let inputs = form.querySelectorAll("input"); // найти все input
+inputs[inputs.length - 1];
+
+// second
+
+let body = document.body;
+
+body.innerHTML = "<!--" + body.tagName + "-->";
+
+alert(body.firstChild.data);
+
+// it will show use comment
